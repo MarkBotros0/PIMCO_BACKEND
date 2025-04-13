a08ee3c1539f576ad4a95e25e0d0226582bb4b29
backend <markbotro0@gmail.com>
2025-04-13 14:56:13 +0200

    add guards to payroll

--- src/payrolls/payrolls.controller.ts
+++ src/payrolls/payrolls.controller.ts
@@ -12,37 +12,37 @@ import { PayrollsService } from './payrolls.service';
 import { UpdatePayrollDto } from './dto/update-payroll.dto';
 import { AccessTokenGuard } from '../auth/guards/access-token.guard';
 import { PayrollQueryDto } from './dto/payroll-query.dto';
-import { HRGuard } from '../auth/guards/hrGuard';
+import { AdminOrHRGuard } from '../auth/guards/admin-or-hr.guard';
 
 @Controller('payrolls')
 export class PayrollsController {
   constructor(private readonly payrollsService: PayrollsService) {}
 
-  @UseGuards(AccessTokenGuard, HRGuard)
+  @UseGuards(AccessTokenGuard, AdminOrHRGuard)
   @Post('submit')
   async submitPayrolls() {
     return this.payrollsService.submitPayrolls();
   }
 
-  @UseGuards(AccessTokenGuard, HRGuard)
+  @UseGuards(AccessTokenGuard, AdminOrHRGuard)
   @Get('past')
   async findAllPast(@Query() query: PayrollQueryDto) {
     return this.payrollsService.findAllPast(query);
   }
 
-  @UseGuards(AccessTokenGuard, HRGuard)
+  @UseGuards(AccessTokenGuard, AdminOrHRGuard)
   @Get('current')
   async findAll(@Query() query: PayrollQueryDto) {
     return this.payrollsService.findAllCurrent(query);
   }
 
-  @UseGuards(AccessTokenGuard, HRGuard)
+  @UseGuards(AccessTokenGuard, AdminOrHRGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.payrollsService.findOne(+id);
   }
 
-  @UseGuards(AccessTokenGuard, HRGuard)
+  @UseGuards(AccessTokenGuard, AdminOrHRGuard)
   @Patch(':id')
   async update(
     @Param('id') id: string,
